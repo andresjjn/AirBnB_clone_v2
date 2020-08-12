36a7bd1bad7377d963f7e9f4f7b54cb9992cf92c
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-12 23:36:17 +0000

    Update classes to implement DBStorage

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,44 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+import uuid
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DataTime, Foreignkey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+
 Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
-            self.id = Column(String(60), nullable=False, primary_key=True)
-            self.created_at = Column(DataTime, nullable=False, default=datetime.utcnow())
-            self.updated_at = Column(DataTime, nullable=False, default=datetime.utcnow())
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+
             for key, value in kwargs.items():
-                if key is not "updated_at" and "created_at":
+                if key != "updated_at" or key != "created_at":
                     setattr(self, key, value)
+
+            if "id" not in kwargs.keys():
+                self.id = str(uuid.uuid4())
+
+            del kwargs['__class__']
+            """ if "created_at" not in kwargs.keys():
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs.keys():
+                self.updated_at = datetime.now() """
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -54,6 +67,6 @@ class BaseModel:
         return dictionary
     
     def delete(self):
-    """Delete the current instance from the storage (models.storage) by calling
-    the method delete"""
+        """Delete the current instance from the storage (models.storage) by calling
+        the method delete"""
         storage.delete(self)
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """ This module defines a class to manage DataBase for hbnb clone"""
 from os import getenv
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import create_engine
+from models.city import City
+from models.state import State
+from models.base_model import Base
 
 
 class DBStorage():
@@ -13,15 +16,13 @@ class DBStorage():
     def __init__(self):
         """Constructor"""
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format
-                        (getenv(HBNB_MYSQL_USER),
-                        getenv(HBNB_MYSQL_PWD),
-                        getenv(HBNB_MYSQL_HOST),
-                        getenv(HBNB_MYSQL_DB)), pool_pre_ping=True)
-        if getenv(HBNB_ENV) is 'test':
+                        (getenv('HBNB_MYSQL_USER'),
+                        getenv('HBNB_MYSQL_PWD'),
+                        getenv('HBNB_MYSQL_HOST'),
+                        getenv('HBNB_MYSQL_DB')), pool_pre_ping=True)
+        if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
         Base.metadata.create_all(self.__engine)
-        Session = sessionmaker(bind=self.__engine)
-        self.__session = Session()
 
     def all(self, cls=None):
         """Query on the current database session all objects depending
@@ -43,3 +44,31 @@ class DBStorage():
                 key = "{}.{}".format(cls.__name__, element.id)
                 dic[key] = element
             return dic
+
+    def new(self, obj):
+        """ add the object to the current database session """
+        self.__session.add(obj)
+
+    def save(self):
+        """ commit all changes of the current database session """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """ delete from the current database session obj if not None """
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """ create all tables in the database(feature of SQLAlchemy)
+        create the current database session(self.__session)
+        from the engine(self.__engine) by using a sessionmaker """
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+
+
+
+
