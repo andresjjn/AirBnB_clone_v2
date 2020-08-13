a1bd92a919b246ec970a9f6c1b276466fc4b9760
Diana Carolina Quintero <karodev3@gmail.com>
2020-08-13 23:20:10 +0000

    Fix: getter in place.py and state.py

--- models/state.py
+++ models/state.py
@@ -15,7 +15,9 @@ class State(BaseModel, Base):
     @property
     def cities(self):
         """Getter of cities"""
+        from models import storage
         list_city = []
         for city in storage.all(City):
             if self.id is city.state_id:
                 list_city.append(city)
+        return list_city
