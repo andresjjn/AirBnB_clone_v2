4ffca135cbee32f85d6748fe09d6e3d9dc206dff
Andres Felipe Jejen Tabares <andresjt93@gmail.com>
2020-09-05 20:14:01 +0000

    Update: Improve engines

--- models/state.py
+++ models/state.py
@@ -4,6 +4,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
+from os import getenv
 
 
 class State(BaseModel, Base):
@@ -11,13 +12,13 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
-
-    @property
-    def cities(self):
-        """Getter of cities"""
-        from models import storage
-        list_city = []
-        for city, value in storage.all(City).items():
-            if self.id is value.state_id:
-                list_city.append(value)
-        return list_city
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Getter of cities"""
+            from models import storage
+            list_city = []
+            for city, value in storage.all(City).items():
+                if self.id is value.state_id:
+                    list_city.append(value)
+            return list_city
